0ed29278308796f2a959e96ca258eae3eeb10e11
Shoon <tishinadw@gmail.com>
2023-10-09 10:46:34 +0300

    Добавьте файлы проекта.

---
+++ PersistentConMvc/Models/Data.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersistentConMvc.Models
+{
+    public class Data
+    {
+        public string Name { get; set; } // Имя пользователя
+        public string Message { get; set; } // Сообщение пользователя
+    }
+}
